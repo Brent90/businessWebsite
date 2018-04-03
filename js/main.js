e978568586ab0d6e577965975ab2d7b2c8e45a55
Brent90 <jbaumgart2357@gmail.com>
2018-04-03 13:53:59 -0400

    init commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/*This fixes the navbar menu from covering the showcase text when screen is small */
+/*jslint browser: true*/
+ 
+/*
+
+$(document).ready(function(){
+     var num = 1;
+   $('.toggle').on('click', function(){
+      
+       if(num % 2 ==0){
+            $('#showcase-text-border').css('margin-top', '');
+            num++;
+    } 
+       else if(!num%2==0){
+        $('#showcase-text-border').css('margin-top', '250px');
+        num++;   
+    }
+       
+       
+    });
+   
+       
+    
+   });
+   
+
+
+*/
